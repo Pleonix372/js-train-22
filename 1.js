4c942d3dc4f05b4c41a5c0a9315a63c76cea318a
Pleonix372 <sasha.miroshnikov372@gmail.com>
2023-08-17 11:53:59 +0300

    js-train-22

--- 1.js
+++ 1.js
@@ -5,12 +5,21 @@
 class OrderTracker {
   // Приватне статичне instance поле для збереження єдиного екземпляра класу початкове значення null
   // Приватне статичне orders поле для збереження списку замовлень початкове значення []
+  static #instance = null;
+  static #orders = [];
   /**
    * Статичний метод create використовується для створення єдиного екземпляра класу
    */
   // Перевіряємо, чи є вже створений екземпляр класу
   // Якщо немає, створюємо новий екземпляр
   // Інакше повертаємо єдиний екземпляр класу
+  static create() {
+    if (!this.#instance) {
+      this.#instance = new OrderTracker();
+    }
+
+    return this.#instance;
+  }
   /**
    * Статичний метод add використовується для додавання замовлення до списку
    * Отримує item та додає його до масиву замовлень
@@ -18,19 +27,26 @@ class OrderTracker {
   /**
    * Статичний метод get використовується для отримання списку замовлень
    */
+  static add(item) {
+    this.#orders.push(item);
+  }
+
+  static get() {
+    return this.#orders;
+  }
 }
 console.log("Завдання 1 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створюємо єдиний екземпляр класу OrderTracker
-// const tracker = OrderTracker.create();
+const tracker = OrderTracker.create();
 
 // Додаємо замовлення до списку
-// OrderTracker.add("Телефон");
-// OrderTracker.add("Ноутбук");
+OrderTracker.add("Телефон");
+OrderTracker.add("Ноутбук");
 
 // Отримуємо список замовлень
-// const orders = OrderTracker.get();
+const orders = OrderTracker.get();
 
 // Виводимо список замовлень в консоль
-// console.log(orders);
+console.log(orders);
